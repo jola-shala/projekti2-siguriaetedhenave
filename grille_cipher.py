48541b340f10d7efcd655646b327bb069b108138
jola-shala <jola.shala@student.uni-pr.edu>
2026-04-15 13:01:19 +0200

    add input

--- grille_cipher.py
+++ grille_cipher.py
@@ -11,6 +11,27 @@ def decrypt_grille(cipher, grille, size):
 
     return ''. join(result)
 
+def input_grille(size):
+    print("Shkruaj grille (0 dhe 1): ")
+    grille = []
+
+    for i in range(size):
+        row_input = input(f"Rreshti {i+1}: ")
+
+        if " " in row_input:
+            row = [int(x) for x in row_input.split()]
+        else:
+            row = [int(x) for x in row_input.strip()]
+
+        if len(row) != size:
+            print("Gabim: Rreshti nuk ka gjatesi te sakte!")
+            exit()
+
+        grille.append(row)
+    
+    return grille
+
+
 
 
 
