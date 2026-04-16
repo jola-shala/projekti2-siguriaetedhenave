088a4c00c5014c3ebe00fe65f48c769c1db2ef85
jola-shala <jola.shala@student.uni-pr.edu>
2026-04-16 13:44:32 +0200

    add

--- grille_cipher.py
+++ grille_cipher.py
@@ -31,6 +31,28 @@ def input_grille(size):
     
     return grille
 
+if __name__ == "__main__":
+    print("=== Grille Cipher ===")
+    print("1. Encrypt")
+    print("2. Decrypt")
+
+    choice = input("Zgjedh opsionin (1/2): ")
+    size = int(input("Shkruaj madhesine e matrices (vetem numer p.sh 3): ").strip())''
+    
+    grille = input_grille(size)
+
+    if choice == "1":
+        text = input("Shkruaj tekstin per enkriptim: ")
+        encrypted = encrypt_grille(text, grille, size)
+        print("Encrypted:", encrypted)
+
+    elif choice == "2":
+        cipher = input("Shkruaj tekstin per dekriptim: ")
+        decrypted = decrypt_grille(cipher, grille, size)
+        print("Decrypted:", decrypted)
+
+    else:
+        print("Opsioni i pavlefshem!")   
 
 
 
