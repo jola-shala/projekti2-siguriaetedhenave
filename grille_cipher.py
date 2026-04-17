2e237eaddea49c9f062d28ead75052db8aafe4f9
jola-shala <jola.shala@student.uni-pr.edu>
2026-04-17 10:34:49 +0200

    addgrille

--- grille_cipher.py
+++ grille_cipher.py
@@ -9,7 +9,32 @@ def generate_grille(size):
     positions = []
     total_holes = (size * size) 
 
-    
+    while len(positions) < total_holes:
+        i = random.randint(0, size-1)
+        j = random.randint(0, size-1)
+
+        g = [[0]*size for _ in range(size)]
+        g[i][j] = 1
+
+        ok = True
+        temp = g
+
+        for _ in range(4):
+            for x in range(size):
+                for y in range(size):
+                    if temp[x][y] == 1 and (x, y) in positions:
+                        ok = False
+            temp = rotate(temp)
+
+        if ok:
+            positions.append((i, j))
+
+    for (i, j) in positions:
+        grille[i][j] = 1
+
+    return grille
+
+
 
 
 
